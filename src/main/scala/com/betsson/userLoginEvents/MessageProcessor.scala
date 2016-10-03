5fe7b8148b83285dfd84289794c5733f08b5019f
sunilporal <sunil.poral@gmail.com>
2016-10-03 01:11:51 -0700

    Update MessageProcessor.scala

--- src/main/scala/com/betsson/userLoginEvents/MessageProcessor.scala
+++ src/main/scala/com/betsson/userLoginEvents/MessageProcessor.scala
@@ -1,46 +1,50 @@
 
 package com.betsson.userLoginEvents
 
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.rdd.RDD
+import scala.util.parsing.json._
+
 import org.apache.spark.Accumulator
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.SaveMode
+
 
 object MessageProcessor extends Serializable {
   
   val CLASS_NAME = this.getClass
 
   @transient lazy val log = org.apache.log4j.LogManager.getLogger(CLASS_NAME)
-
+ // implicit val formats = net.liftweb.json.DefaultFormats
   def processStream(accumulator: Accumulator[Long], sqlContext: SQLContext, rdd: RDD[String]) {
     
     try{
       
       if (!rdd.isEmpty() && null!=sqlContext) {
         
+       rdd.map { x => new JSONObject(x).getJSONBoolean("IsSuccessful")}
+         .filter { x => 
+           if(x == false){
+           sqlContext.read.json(rdd)
+           .select("customerid", "brandname", "timestamp","issuccessful")
+           .write
+           .format("org.apache.spark.sql.cassandra")
+           .options(Map("table" -> "userlogins", "keyspace" -> "betsson"))
+           .mode(SaveMode.Append)
+           .save()
+           
+           }
+            
+         else{
+          sqlContext.sql("DELETE from userlogins where CustomerId ==" + new JSONObject(x).getJSONString("CustomerID"))
+        }}
         
-        
-       
-        sqlContext.read.json(rdd)
-        .select("customerid", "brandname", "timestamp","issuccessful")
-        .write
-        .format("org.apache.spark.sql.cassandra")
-        .options(Map("table" -> "userlogins", "keyspace" -> "betsson"))
-        .mode(SaveMode.Append)
-        .save()
-        
-        
-        
-        
-       /* customer.registerTempTable("UserLogins")
-        
-       sqlContext.sql("insert into table UserLogins values(\"00002c28-7599-11e1-8028‐1cc1dee5ebd\",\"TestBrand\",\"2016‐05‐31T09:06:33.303\",true)")
-       
+
         val cust = sqlContext.sql("SELECT CustomerId FROM UserLogins group by CustomerId having count(CustomerId) > 2")
         println("*****************************"+cust.show() + "**********************************")
         
-        cust.foreach { x => FailedEventPublish.send(x.getAs("CustomerId")) }
-        */
+        cust.foreach { x => FailedEventPublish.send(x.getAs("CustomerId"))
+        }
+
         
       }
       
@@ -65,6 +69,7 @@ object MessageProcessor extends Serializable {
   
   
 }
+
 
 
 
